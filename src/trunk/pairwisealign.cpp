699455154fe22f624b70546739d942b99a97e954
teqdruid <teqdruid@7a37da0a-1d71-11de-b650-e715bd6d7cf1>
2009-04-28 21:17:02 +0000

    Slimming down PWA and blah..

--- src/trunk/pairwisealign.cpp
+++ src/trunk/pairwisealign.cpp
@@ -20,7 +20,7 @@ public:
 	delete[] mat;
     }
 
-    T& operator()(size_t x, size_t y) {
+    __inline__ T& operator()(size_t x, size_t y) {
 	return mat[x + y*xs];
     }
 };
@@ -34,40 +34,48 @@ public:
 template<class M, class A, class B>
 double alignmentScore(M& S, A& a, B& b, SiteInformation* si = NULL)
 {
-    Matrix<double> F(a.length()+1, b.length()+1, 0.0);
-    Matrix<size_t> vGaps(a.length()+1, b.length()+1, 0);
-    Matrix<size_t> hGaps(a.length()+1, b.length()+1, 0);
+    vector<double> Fc(b.length()+1), Fb(b.length()+1);
+    vector<size_t> hGapsB(b.length()+1), hGapsC(b.length()+1);
+
+    //Backtracking directions
     Matrix<char>   dirct(a.length()+1, b.length()+1, 'S');
 
-    for (size_t i=0; i<=a.length(); ++i) {
-	F(i, 0) = S.hAffineScore(i);
-	hGaps(i, 0) = i;
-    }
+    size_t vGaps = 0;
+    hGapsB[0] = 0;
 
     for (size_t j=0; j<=b.length(); ++j) {
-	F(0, j) = S.vAffineScore(j);
-	vGaps(0, j) = j;
+	Fb[j] = S.vAffineScore(j);
     }
 
     for (size_t i = 1; i <= a.length(); ++i) {
+	Fc[0] = S.hAffineScore(i);
+	hGapsC[0] = i; 
+
 	for (size_t j = 1; j <= b.length(); ++j) {
-	    double diag = F(i-1, j-1) + S(a[i-1], b[j-1]);
-	    double horz = F(i-1, j)   + S.hAffineScore(hGaps(i-1, j) + 1);
-	    double vert = F(i, j-1)   + S.vAffineScore(vGaps(i, j-1) + 1);
+	    double diag = Fb[j-1] + S(a[i-1], b[j-1]);
+	    double horz = Fb[j]   + S.hAffineScore(hGapsB[j] + 1);
+	    double vert = Fc[j-1] + S.vAffineScore(vGaps + 1);
 
 	    if (diag > horz && diag > vert) {
-		F(i, j) = diag;
+		Fc[j] = diag;
 		dirct(i, j) = 'D';
+		hGapsC[j] = 0;
+		vGaps = 0;
 	    } else if (horz > vert) {
-		F(i, j) = horz;
-		hGaps(i, j) = hGaps(i-1, j) + 1;
+		Fc[j] = horz;
+		hGapsC[j] = hGapsB[j] + 1;
+		vGaps = 0;
 		dirct(i, j) = 'H';
 	    } else {
-		F(i, j) = vert;
-		vGaps(i, j) = vGaps(i, j-1) + 1;
+		Fc[j] = vert;
+		vGaps++;
+		hGapsC[j] = 0;
 		dirct(i, j) = 'V';
 	    }
 	}
+
+	Fc.swap(Fb);
+	hGapsC.swap(hGapsB);
     }
 
     if (si != NULL) {
@@ -111,7 +119,7 @@ double alignmentScore(M& S, A& a, B& b, SiteInformation* si = NULL)
 	}
     }
 
-    return F(a.length(), b.length());
+    return Fb[b.length()];
 }
 
 
